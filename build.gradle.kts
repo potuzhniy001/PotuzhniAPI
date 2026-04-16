bad6df87c110bf0c962d156670a2820f9e841e3c
FarmMan <kusilla16@gmail.com>
2026-04-16 19:55:59 +0300

    commit 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,12 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven ("https://repo.extendedclip.com/releases/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java {
--- src/main/java/io/github/potuzhniAPI/menu/Menu.java
+++ src/main/java/io/github/potuzhniAPI/menu/Menu.java
@@ -1,4 +1,37 @@
 package io.github.potuzhniAPI.menu;
 
-public interface Menu {
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public interface Menu extends InventoryHolder {
+
+    void click(Player player, int slot);
+
+    void setItem(int slot, ItemStack item);
+
+    void setItem(int slot, ItemStack item, Consumer<Player> action);
+
+    void onSetItems();
+
+    boolean usePlaceholders();
+
+    void setPlaceholders();
+
+    void update();
+
+    Map<Integer, ItemStack> getItemsMap();
+    Map<Integer, Consumer<Player>> getActionsMap();
+
+    default void open(Player player) {
+        if (usePlaceholders())
+            setPlaceholders();
+
+        onSetItems();
+        player.openInventory(getInventory());
+    }
+
 }
